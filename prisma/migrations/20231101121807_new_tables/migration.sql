32434536ada5bf59172799c8131b14d5e57bd772
alibukhari1995 <95312876+alibukhari1995@users.noreply.github.com>
2023-11-06 01:07:44 +0500

    bccc

--- prisma/migrations/20231101121807_new_tables/migration.sql
+++ prisma/migrations/20231101121807_new_tables/migration.sql
@@ -1,4 +1,51 @@
 -- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `shop` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `isOnline` BOOLEAN NOT NULL DEFAULT false,
+    `scope` VARCHAR(191) NULL,
+    `expires` DATETIME(3) NULL,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `userId` BIGINT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Shop` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `shop` VARCHAR(191) NOT NULL,
+    `plan` INTEGER NULL,
+    `baseLanguageCode` VARCHAR(191) NULL,
+    `TargetLanguagesCode` VARCHAR(191) NULL,
+    `wordsUsed` INTEGER NULL DEFAULT 0,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Charge` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `shopifyId` VARCHAR(191) NOT NULL,
+    `confirmationUrl` VARCHAR(191) NOT NULL,
+    `shop` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'PENDING',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Page` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `pageId` VARCHAR(191) NOT NULL,
+    `language` VARCHAR(191) NOT NULL,
+    `translation` LONGTEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `HumanPageStore` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `shop` VARCHAR(191) NOT NULL,
